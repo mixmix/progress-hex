1d9d6f5d4d51911097d83d043f14b2b31a817912
mixmix <whimful@gmail.com>
2019-03-19 18:15:53 +1300

    save-point

--- test/view/index.js
+++ test/view/index.js
@@ -11,8 +11,20 @@ svg.style.setProperty('width', '100%')
 svg.style.setProperty('max-height', '100vh')
 document.body.appendChild(svg)
 
-const r = 4
-var n = 1
+const r = 5
+// var n = 1
+var n = 150
+var progress = 75
+
+setInterval(
+  () => n++,
+  50
+)
+
+setInterval(
+  () => progress++,
+  90
+)
 
 setInterval(
   () => {
@@ -21,28 +33,30 @@ setInterval(
         viewBox: '-300 -200 600 400',
         height: '100vh'
       },
-      hexGrid({ r, n }).map((vec, i) => Circle({ vec, r, i }))
+      hexGrid({ r, n }).map((vec, i) => Circle({ vec, r, i, progress }))
     )
     newSvg.style.setProperty('width', '100%')
     newSvg.style.setProperty('max-height', '100vh')
 
     document.body.replaceChild(newSvg, svg)
     svg = newSvg
-
-    n++
   },
   50
 )
 
-function Circle ({ vec, r, i }) {
+function Circle ({ vec, r, i, progress }) {
   return h('circle', {
     cx: vec[0],
     cy: vec[1],
-    r: r - 1,
-    // fill: 'rgb(255, 100, 255)',
+    r: i <= progress ? r - 1 : 2,
     fill: `hsl(${(i / 4 + 180) % 360}, 70%, 70%)`
-    // fill: 'rgb(0,0,0)',
-    // 'data-dist': vecDist(vec)
+    // fill: i <= progress
+    //   ? `hsl(${(i / 4 + 180) % 360}, 70%, 70%)`
+    //   : 'none',
+    // stroke: `hsl(${(i / 4 + 180) % 360}, 70%, 70%)`,
+    // 'stroke-width': 1,
+    // 'data-dist': vecDist(vec),
+    // style: { opacity: i >= progress ? 0.2 : 1 }
   })
 }
 
